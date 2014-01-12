dc90788b13d599942695cd47abeeab9d1712a4c8
Jiri Balaty <jiri.balaty@gmail.com>
2014-01-12 20:00:53 +0100

    Small changes

--- TODO.rb
+++ TODO.rb
@@ -1,5 +1,5 @@
 # it is called .rb else Webstorm will not show it in it's td window
 #TODO: chech conditions on free accounts
 #    - only one add per email
-#TODO: spam protection - SPF an maybe DKIM
+#TODO: spam protection - SPF an maybe DKIM / Mailchimp integration
 #TODO: rewrite all links and email addresses from PCIN.cz and rwatcher.* to proper domain
