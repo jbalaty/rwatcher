bfa40208ec37b3dde2bf34d4e04ad57e708432fe
Jiri Balaty <jiri.balaty@gmail.com>
2014-01-13 19:41:49 +0100

    Small changes in texts

--- TODO.rb
+++ TODO.rb
@@ -1,5 +1,4 @@
 # it is called .rb else Webstorm will not show it in it's td window
 #TODO: chech conditions on free accounts
 #    - only one add per email
-#TODO: spam protection - SPF an maybe DKIM / Mailchimp integration
 #TODO: rewrite worker - better scrapping algorithm
