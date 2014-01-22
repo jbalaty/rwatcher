b82122490ab9b4f93bcc2c837d8eac17846de2de
Jiri Balaty <jiri.balaty@gmail.com>
2014-01-23 00:06:53 +0100

    Add commiting of precompiled assets

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #/bin/sh
 
+echo 'Compiling assets'
 RAILS_ENV=production
 rake assets:precompile
+if [ "$?" == "0"  ]
+then
+	BRANCH=`git rev-parse --abbrev-ref HEAD`
+	echo "Forcing assets add into current branch ($BRANCH)"
+	git add public/assets --force
+	git commit -m"Build script - precompiled assets"
+fi
